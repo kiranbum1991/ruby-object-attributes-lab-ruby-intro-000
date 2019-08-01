9bbe97c72ed203a8bbc381f8098af08c5c13f377
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-01 18:37:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+  def name
+   @name = name
+  end
+
+  def name=(name)               # creates setter method for age
+     @name = name
+  end
+
+  def job
+   @job = job
+  end
+
+  def job=(job)               # creates setter method for age
+     @job = job
+  end
 
 end
